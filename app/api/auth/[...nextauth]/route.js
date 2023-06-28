fccddc65a9faeb595dd5cc78411a41046f9459a6
Nasuha Asri <nasuhasri00@gmail.com>
2023-06-29 01:02:23 +0800

    add callback key in NextAuth function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -18,42 +18,44 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  // function - to keep data about the user every single time
-  // reason: to keep an existing and running session
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    })
-
-    session.user.id = sessionUser._id.toString();
-
-    return session; // know which user currently online
-  },
-  async signIn({ profile }) {
-    // every next js route is serverless route
-    // serverless -> lambda function -> dynamodb : open up when its get called
-    try {
-      await connectToDB();
-
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email // check by email
+  callbacks: {
+    // function - to keep data about the user every single time
+    // reason: to keep an existing and running session
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
       })
-
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profie.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
+  
+      session.user.id = sessionUser._id.toString();
+  
+      return session; // know which user currently online
+    },
+    async signIn({ profile }) {
+      // every next js route is serverless route
+      // serverless -> lambda function -> dynamodb : open up when its get called
+      try {
+        await connectToDB();
+  
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email // check by email
         })
+  
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          })
+        }
+  
+        // successfully sign in, return true
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false
       }
-
-      // successfully sign in, return true
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false
     }
   }
 })
